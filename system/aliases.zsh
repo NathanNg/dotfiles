3640a4f5705da5d04e4f3f6503b397497910b68c
Boong <Nathan.Ng@gmail.com>
2015-01-12 22:24:19 -0600

    create showFiles and hideFiles alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,7 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
